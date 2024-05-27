7b64f28615e98fcf6b21c6578ab73c293ae8d058
snjv9 <snjvchoudhary9@gmail.com>
2024-05-27 18:41:56 +0530

    Modified test case

--- test/index.test.js
+++ test/index.test.js
@@ -2,55 +2,8 @@ const appServer = require('../server')
 const request = require('supertest')
 
 
-// describe('Test the root path', () => {
-//     test('It should response the GET method', async () => {
-//         const response = await request(app).get('/');
-//         expect(response.statusCode).toBe(404);
-//     });
-// });
+
 let userId;
-describe('Test user CRUD operations', () => {
-
-    test('It should create a new user', async () => {
-        const response = await request(appServer)
-            .post('/api/v1/users')
-            .send({
-                name: 'Test User',
-                email: 'testuser@gmail.com',
-                phoneNumber: '1234567890',
-                password: "password"
-            });
-        expect(response.statusCode).toBe(201);
-        userId = response.body.data._id; // Save the user ID for later tests
-
-    });
-
-    test('It should retrieve all users', async () => {
-        const response = await request(appServer).get('/api/v1/users');
-        expect(response.statusCode).toBe(200);
-    });
-
-    test('It should retrieve a specific user', async () => {
-        const response = await request(appServer).get(`/api/v1/users/${userId}`);
-        expect(response.statusCode).toBe(200);
-    });
-
-    test('It should update a specific user', async () => {
-        const response = await request(appServer)
-            .patch(`/api/v1/users/${userId}`)
-            .send({
-                name: 'Updated Test User'
-            });
-        expect(response.statusCode).toBe(200);
-    });
-
-    test('It should delete a specific user', async () => {
-        const response = await request(appServer).delete(`/api/v1/users/${userId}`);
-        expect(response.statusCode).toBe(204);
-    });
-
-
-});
 
 describe('Auth Routes', () => {
     let token;
@@ -66,6 +19,53 @@ describe('Auth Routes', () => {
         expect(res.body).toHaveProperty('token');
         token = res.body.token;
     });
+
+
+    describe('Test user CRUD operations', () => {
+
+        test('It should create a new user', async () => {
+            const response = await request(appServer)
+                .post('/api/v1/users')
+                .send({
+                    name: 'Test User',
+                    email: `testuser${Math.floor(Math.random() * 1000)}@gmail.com`,
+                    phoneNumber: '1234567890',
+                    password: "password"
+                });
+            expect(response.statusCode).toBe(201);
+            userId = response.body.data._id; // Save the user ID for later tests
+
+        });
+
+        test('It should retrieve all users', async () => {
+            const response = await request(appServer).get('/api/v1/users');
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should retrieve a specific user', async () => {
+            const response = await request(appServer).get(`/api/v1/users/${userId}`);
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should update a specific user', async () => {
+            const response = await request(appServer)
+                .patch(`/api/v1/users`)
+                .send({
+                    name: 'Updated Test User'
+                }).set('Authorization', `Bearer ${token}`);
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should delete a specific user', async () => {
+            const response = await request(appServer).delete(`/api/v1/users`).set('Authorization', `Bearer ${token}`);
+            expect(response.statusCode).toBe(204);
+        });
+    });
+
+
+
+
+
     let todoId;
     describe('Test Todo CRUD operations', () => {
 
